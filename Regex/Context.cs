d463e298c440a0eb8e337b4c738f56f62c68e87e
CanftIn <wwc7033@gmail.com>
2018-03-02 16:02:00 +0800

    Initial commit

---
+++ Regex/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex
+{
+    public class Context
+    {
+        public readonly string matchString;
+        public int offset = 0;
+
+        public Context(string matchString)
+        {
+            this.matchString = matchString;
+        }
+
+        public int length => matchString.Length;
+
+        public override string ToString()
+        {
+            string str = matchString + '\n' + (new string('-', offset)) + '^';
+            return str;
+        }
+
+    }
+}
---
+++ RegexTest/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Regex;
+
+namespace RegexTest
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            var c = new Context("abcde");
+
+            Console.WriteLine(c);
+
+            c.offset = 2;
+            Console.WriteLine(c);
+
+        }
+    }
+}
